b1bd9b4832a1a34234a78873935691a173df8d52
hasinla <84169342+hasinla@users.noreply.github.com>
2023-04-05 04:36:15 +0900

    [Bronze V] Title: 개, Time: 152 ms, Memory: 9324 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.js
@@ -0,0 +1,10 @@
+const fs = require('fs'); 
+const input = fs.readFileSync('dev/stdin').toString().split(' ');//입력 값을 받아옴
+
+
+
+console.log(`|\\_/|
+|q p|   /}
+( 0 )"""\\
+|"^"\`    |
+||_/=\\\\__|`)

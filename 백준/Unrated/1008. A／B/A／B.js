3aadff801841d041778685c5b8e447fab6869548
hasinla <84169342+hasinla@users.noreply.github.com>
2023-04-04 19:34:34 +0900

    [Unrated] Title: A/B, Time: 124 ms, Memory: 9592 KB -BaekjoonHub

---
+++ 백준/Unrated/1008. A／B/A／B.js
@@ -0,0 +1,8 @@
+const fs = require('fs'); 
+const input = fs.readFileSync("/dev/stdin").toString().split(" ");//입력 값을 받아옴
+
+
+const A = parseInt(input[0]); // 변수 A 선언
+const B = parseInt(input[1]); // 변수 B 선언
+
+console.log(A / B); // 출력
